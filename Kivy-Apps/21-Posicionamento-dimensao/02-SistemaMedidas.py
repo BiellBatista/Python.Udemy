e565680e4505a990e4ad174dc8dd007d12421b94
Gabriel Batista <gabriel.texteis@gmail.com>
2026-09-07 01:28:16 -0300

    conclusão da aula 21 - Posicionamento e dimensão

---
+++ Kivy-Apps/21-Posicionamento-dimensao/02-SistemaMedidas.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+'''
+Kivy aceita essas medidas
+
+px - Pixel
+m - Polegadas
+mm - Milímetros
+pt - Pontos
+dp - Pixel Independente de Densidade
+sp - Escala Independente de Pixel
+'''
+
+#Pixel é a unidade de medida universal utilizado pelos fabricantes de hardware e linguagem de programação.
+'''
+Resolução de tela é a quantidade de pixel do display
+'''
+#dpi (dots per inch) ou pontos por polegadas é a medida obtida pela divisão do tamanho físico do display em centímetros ou polegadas pela resolução em pixels.
+#densidade de pixel é a quantidade de pixels por polegada quadrada
+
+#dp para icones e sp para fontes
